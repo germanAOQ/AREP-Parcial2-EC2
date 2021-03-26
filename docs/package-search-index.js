092aa3c6ec979f54c664a3427d4f4d4777ae154d
germanAOQ <60035085+germanAOQ@users.noreply.github.com>
2021-03-26 17:20:21 -0500

    Documentación

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"edu.escuelaing.arep.parcial"},{"l":"edu.escuelaing.arep.parcial.model"},{"l":"edu.escuelaing.arep.parcial.service"},{"l":"edu.escuelaing.arep.parcial.trigonometricServer"}];updateSearchResults();
